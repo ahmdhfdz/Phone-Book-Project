532638782bff3a478fa537b703d592ed4d4d1a75
hafidz <ahmad.hafidz98@yahoo.co.id>
2022-10-04 19:29:48 +0700

    add Fuctionality and Form Create New Contact

--- src/pages/FormContact.tsx
+++ src/pages/FormContact.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import FormAdd from '../components/FormAdd';
+import styled from '@emotion/styled';
+
+const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
 const FormContact = () => {
   return (
-    <div>
-        <h2>Add Form Contact</h2>
-    </div>
+    <FormWrapper>
+      <div>
+        <h2>Add New Contact</h2>
+        <FormAdd />
+      </div>
+    </FormWrapper>
   )
 }
 
