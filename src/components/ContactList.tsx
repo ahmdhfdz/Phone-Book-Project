2a960054948fca0cc389b77b5ff2bfee8c96875e
hafidz <ahmad.hafidz98@yahoo.co.id>
2022-10-04 19:32:39 +0700

    add button delete and favorite

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react'
 import { useContactList } from '../GraphQL/GetContactList'
 import styled from '@emotion/styled'
 import { useNavigate } from 'react-router-dom'
+import { AiFillDelete } from 'react-icons/ai'
+import { BsFillBookmarkStarFill } from 'react-icons/bs'
+import { DELETE_CONTACT } from '../GraphQL/deleteContact'
 
 const ListWrapper = styled.ul`
     padding: 0;
@@ -13,7 +16,6 @@ const ListItem = styled.li`
   list-style-type: none;
   flex: 0 0 50%;
   text-align: center;
-  cursor: pointer;
 
   @media (min-width: 420px){
     flex: 0 0 20%;
@@ -27,17 +29,35 @@ const ListContent = styled.div`
   border-radius: 5px;
   padding: 10px 0;
   min-height: 100px;
+  position: relative;
 `
 
-function ContactList() {
+const DeleteBtn = styled.div`
+  :hover{
+    color: red;
+  }
+  padding: 10px;
+`
 
-  const { error, loading, data } = useContactList(10, 0)
+const FavBtn = styled.div`
+  :hover{
+    color: gold;
+  }
+  padding: 10px;
+`
+
+const ContainerBtn = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  cursor: pointer;
+  `
+
+function ContactList(props: any) {
+
+  const { error, loading, data } = useContactList(10, props.offset)
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    
-  }, [data])
 
   return (
     <ListWrapper>
@@ -56,12 +76,24 @@ function ContactList() {
       {
         data &&
         data.contact.map((items: any) => {
-          return <ListItem key={items.id} onClick={() => navigate('contact-detail')}>
+          return <ListItem key={items.id} 
+          // onClick={() => navigate('contact-detail')}
+          >
             <ListContent>
-              <h4>Name: {items.first_name} {items.last_name} </h4>
               <div>
-                Number: {items.phones[0].number}
+                <h4>Name: {items.first_name} {items.last_name} </h4>
+                <div>
+                  Number: {items.phones[0].number}
+                </div>
               </div>
+              <ContainerBtn>
+                <FavBtn onClick={()=>console.log("favorite")}>
+                  <BsFillBookmarkStarFill />
+                </FavBtn>
+                <DeleteBtn onClick={()=>console.log("delete", items.id)}>
+                  <AiFillDelete />
+                </DeleteBtn>
+              </ContainerBtn>
             </ListContent>
           </ListItem>
         })
